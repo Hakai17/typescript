5569033f8b49a190ed5395cdb5516a3d95b722a5
Hakai17 <miguel.costa.arruda08@aluno.ifce.edu.br>
2023-03-09 15:20:18 -0300

    create trades file

---
+++ app/models/trades.ts
@@ -0,0 +1,5 @@
+import { Trade } from './trade';
+
+export class Trades {
+    private trade: Array<Trade> = [];
+}
---
+++ dist/js/models/trades.js
@@ -0,0 +1,5 @@
+export class Trades {
+    constructor() {
+        this.trade = [];
+    }
+}
